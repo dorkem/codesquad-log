7ffdbdb0af0d93f6d054a50d055495bf753feb55
dorkem <20233144@gs.cwnu.ac.kr>
2025-08-13 15:46:30 +0900

    :fire: 불필요한 코드 삭제

--- solution/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BoardGame game = new BoardGame();
-        game.start(3);
-        game.turn("A");
-        game.turn("B");
-        game.turn("C");
-    }
-}
